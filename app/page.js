ea678ac19163cf6d38d7be797c9637d8b8932e1a
Nishant <er.nishant.ku1999@gmail.com>
2025-08-20 09:05:30 +0530

    test reploy

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: "40px", textAlign: "center" }}>
       <h1>🚀 Hello from Next.js</h1>
-      <p>This is my first Next.js app deployed on Vercel!</p>
+      <p>This is my first Next.js app deployed on Vercel! Vikash</p>
     </main>
   );
 }
